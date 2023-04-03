fc01b2c92f997aaf3a6ad170f3ccc01cd38df690
Ander Cabrera L <andercabrerac@gmail.com>
2023-04-03 00:54:03 -0600

    modal done. TODO: update function

---
+++ src/js/components/update-reminder.js
@@ -0,0 +1,12 @@
+export default class UpdateReminder {
+  constructor() {
+    this.modalMain = document.getElementById('update-modal');
+    this.title = document.getElementById('title-modal');
+    this.description = document.getElementById('description-modal');
+    this.updateButton = document.getElementById('update-btn');
+    this.cancelBtn = document.getElementById('cancel-button');
+    this.continueBtn = document.getElementById('continue-button');
+  }
+
+  onClick(reminder) {}
+}
